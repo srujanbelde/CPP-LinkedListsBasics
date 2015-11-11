8e4a23af75b3c85ed266b97b6d711607015a902f
srujanbelde <srujanbelde18@gmail.com>
2015-11-11 19:22:12 +0530

    Submit Commit

--- src/twodigitLinkedList.cpp
+++ src/twodigitLinkedList.cpp
@@ -22,5 +22,14 @@ struct node {
 };
 
 int convert_sll_2digit_to_int(struct node *head){
-	return 0;
+	int num=0;
+	struct node *ptr;
+	ptr = head;
+	while (ptr != NULL)
+	{
+		num = num * 10 + (ptr->digit1);
+		num = num * 10 + (ptr->digit2);
+		ptr = ptr->next;
+	}
+	return num;
 }
