95fffe7b3835948d659d5056276317c535e3897c
srujanbelde <srujanbelde18@gmail.com>
2015-11-11 18:33:58 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -21,6 +21,40 @@ struct node {
 	struct node *next;
 };
 
-void sll_012_sort(struct node *head){
-	
+void sll_012_sort(struct node *head)
+{
+	int count0=0,count1=0,count2=0,i=0; 
+	struct node *ptr = head;
+	while (ptr != NULL)
+	{
+		if (ptr->data == 0)
+			count0++;
+		else if (ptr->data == 1)
+			count1++;
+		else
+			count2++;
+		ptr = ptr->next;
+	}
+	ptr = head;
+	while (ptr != NULL)
+	{
+		while (count0 > 0)
+		{
+			ptr->data = 0;
+			--count0;
+			ptr = ptr->next;
+		}
+		while (count1 > 0)
+		{
+			ptr->data = 1;
+			--count1;
+			ptr = ptr->next;
+		}
+		while (count2 > 0)
+		{
+			ptr->data = 2;
+			--count2;
+			ptr = ptr->next;
+		}
+	}
 }
